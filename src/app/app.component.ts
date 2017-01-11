eb829da99dd3c1f73fe8f3ce493b45075b94ca84
Samuel Mello <samueljmello@gmail.com>
2017-01-10 23:10:28 -0500

    Loader changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { 
+    Event as RouterEvent,
+    NavigationStart,
+    NavigationEnd,
+    NavigationCancel,
+    NavigationError,
+    Router 
+} from '@angular/router';
 import '../../public/css/styles.css';
 @Component({
   selector: 'universal-angular-demo',
-  template: `<router-outlet><md-spinner></md-spinner></router-outlet>`
+  template: `
+  <router-outlet>
+  <div class="loading-overlay" *ngIf="loading">
+    <md-spinner></md-spinner>
+  </div>
+  </router-outlet>
+  `
 })
 export class AppComponent {
 
-  constructor(private router: Router) {};
+  loading: boolean = true;
+
+  constructor(private router: Router) {
+    router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event);
+    });
+  };
+
+  public navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationCancel) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
 
 }
